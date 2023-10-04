efc4303b4e00a85d59f6fa670918e39db526bb27
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-10-03 19:32:41 -0500

    first

---
+++ test/unit/DeployMoodNftTest.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public developer;
+
+    function setUp() public {
+        developer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToUri() public view {
+        string
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciLz4=";
+        string memory svg = '<svg xmlns="http://www.w3.org/2000/svg" />';
+        string memory actualUri = developer.svgToImageURI(svg);
+        assert(
+            keccak256(abi.encodePacked(actualUri)) ==
+                keccak256(abi.encodePacked(expectedUri))
+        );
+    }
+}
